d83253a5329abaf72b88beb0d78a052eeb851713
Fabian Graf <fabian@graf.homes>
2024-10-27 00:34:45 +0200

    update authentik

--- kubernetes/apps/security/authentik/app/configuration/scopes.tf
+++ kubernetes/apps/security/authentik/app/configuration/scopes.tf
@@ -1,4 +1,4 @@
-data "authentik_scope_mapping" "oidc-scopes-vikunja" {
+data "authentik_property_mapping_provider_scope" "oidc-scopes-vikunja" {
   managed_list = [
     "goauthentik.io/providers/oauth2/scope-email",
     "goauthentik.io/providers/oauth2/scope-openid",
@@ -6,7 +6,7 @@ data "authentik_scope_mapping" "oidc-scopes-vikunja" {
   ]
 }
 
-data "authentik_scope_mapping" "oidc-scopes-pgadmin" {
+data "authentik_property_mapping_provider_scope" "oidc-scopes-pgadmin" {
   managed_list = [
     "goauthentik.io/providers/oauth2/scope-email",
     "goauthentik.io/providers/oauth2/scope-openid",
@@ -14,7 +14,15 @@ data "authentik_scope_mapping" "oidc-scopes-pgadmin" {
   ]
 }
 
-data "authentik_scope_mapping" "oidc-scopes-mealie" {
+data "authentik_property_mapping_provider_scope" "oidc-scopes-paperless" {
+  managed_list = [
+    "goauthentik.io/providers/oauth2/scope-email",
+    "goauthentik.io/providers/oauth2/scope-openid",
+    "goauthentik.io/providers/oauth2/scope-profile"
+  ]
+}
+
+data "authentik_property_mapping_provider_scope" "oidc-scopes-mealie" {
   managed_list = [
     "goauthentik.io/providers/oauth2/scope-email",
     "goauthentik.io/providers/oauth2/scope-openid",
